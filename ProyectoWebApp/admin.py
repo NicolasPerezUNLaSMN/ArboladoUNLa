68c5177c30e8efea02e45108321b4db8750a1b6d
Nicolas Perez <nykolaiperez@gmail.com>
2021-02-23 18:03:56 -0300

    Borrando imagenes de mas

--- ProyectoWebApp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from ProyectoWebApp.models import Censo, Coordenada, Arbol, EstadoDelArbol, Imagen
-
-# Register your models here.
-admin.site.register(Censo)
-admin.site.register(Coordenada)
-admin.site.register(Arbol)
-admin.site.register(EstadoDelArbol)
-admin.site.register(Imagen)

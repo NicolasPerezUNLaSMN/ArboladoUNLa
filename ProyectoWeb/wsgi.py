037f6957b65d854a6f05e00844ad1aa4a26f27bf
Nicolas Perez <nykolaiperez@gmail.com>
2021-02-24 12:04:05 -0300

    Cling y requisitos

--- ProyectoWeb/wsgi.py
+++ ProyectoWeb/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kloudless.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ProyectoWeb.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
